c7dbf33acbad677c5788016214fce0f8666e9c03
gilangrdhan <gilangrdhan@gmail.com>
2024-10-22 15:03:15 +0700

    company website

---
+++ admin/koneksi.php
@@ -0,0 +1,14 @@
+<?php 
+$host_koneksi ="localhost";
+$username_koneksi = "root";
+$password_koneksi = "";
+$database_koneksi = "angkatan3_compro";
+
+// menghubungkan ke koneksi
+$koneksi = mysqli_connect($host_koneksi, $username_koneksi, $password_koneksi, $database_koneksi); 
+
+if (!$koneksi){
+    echo "Koneksi Gagal";
+}
+
+?>
